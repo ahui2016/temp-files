94258a99e225447af19e7245d3eca9b4e03026ff
ahui2016 <ahui.2015@icloud.com>
2023-08-18 22:57:48 +0800

    *add* function uploadFile()

--- public/util.js
+++ public/util.js
@@ -1,3 +1,29 @@
+const Util = {}
+
+/**
+ * jElem is a JQuery<HTMLElement>
+ */
+Util.disable = function (jElem) {
+  const nodeName = jElem.prop("nodeName");
+  if (nodeName == "BUTTON" || nodeName == "INPUT") {
+    jElem.prop("disabled", true);
+  } else {
+    jElem.css("pointer-events", "none");
+  }
+};
+
+/**
+ * jElem is a JQuery<HTMLElement>
+ */
+Util.enable = function (jElem) {
+  const nodeName = jElem.prop("nodeName");
+  if (nodeName == "BUTTON" || nodeName == "INPUT") {
+    jElem.prop("disabled", false);
+  } else {
+    jElem.css("pointer-events", "auto");
+  }
+};
+
 /**
  * Get a raw element using document.querySelector
  */
@@ -32,6 +58,35 @@ function p(text) {
 
 function timeNow() {
   return dayjs().format("HH:mm:ss");
+}
+
+/**
+ * Upload file using an HTML input element, FormData and fetch().
+ * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * fetchOptions {url, formData, onSuccess, onError, onAlways}
+ */
+async function uploadFile(fetchOptions) {
+  let resp = null;
+  try {
+    resp = await fetch(fetchOptions.url, {
+      method: "POST",
+      body: fetchOptions.formData,
+    });
+    if (!resp.ok) {
+      throw new Error(`Response NG: [${resp.status}] ${resp.statusText}`);
+    }
+    if (fetchOptions.onSuccess) {
+      fetchOptions.onSuccess(resp);
+    }
+  } catch (err) {
+    if (fetchOptions.onError) {
+      fetchOptions.onError(err);
+    }
+    console.error("Error:", err);
+  }
+  if (fetchOptions.onAlways) {
+    fetchOptions(resp);
+  }
 }
 
 /**
