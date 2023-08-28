643dc56ff830015721c663567089d7ab7bda7365
ahui2016 <ahui.2015@icloud.com>
2023-08-28 22:55:32 +0800

    *change* rename app_config.toml to app_config_default.toml

--- init.go
+++ init.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/ahui2016/temp-files/util"
 	"github.com/pelletier/go-toml/v2"
 	"github.com/samber/lo"
 )
@@ -14,23 +15,33 @@ const (
 )
 
 const (
-	AppConfigTOML    = "app_config.toml"
-	PublicFolderName = "public"
-	FilesFolderName  = "files"
+	AppConfigTOML        = "app_config.toml"
+	AppConfigDefaultTOML = "app_config_default.toml"
+	PublicFolderName     = "public"
+	FilesFolderName      = "files"
 )
 
 var (
-	Separator       = string(filepath.Separator)
-	app_root        = filepath.Dir(executable())
-	app_config      *AppConfig
-	app_config_path = filepath.Join(app_root, AppConfigTOML)
-	public_folder   = filepath.Join(app_root, PublicFolderName)
-	files_folder    = filepath.Join(app_root, FilesFolderName)
+	Separator            = string(filepath.Separator)
+	app_root             = filepath.Dir(executable())
+	app_config           *AppConfig
+	app_config_path      = filepath.Join(app_root, AppConfigTOML)
+	app_cfg_default_path = filepath.Join(app_root, AppConfigDefaultTOML)
+	public_folder        = filepath.Join(app_root, PublicFolderName)
+	files_folder         = filepath.Join(app_root, FilesFolderName)
 )
 
 func init() {
+	initAppConfig()
 	readAppConfig()
 	fmt.Println(app_config)
+}
+
+func initAppConfig() {
+	if util.PathNotExists(app_config_path) {
+		err := util.CopyFile(app_config_path, app_cfg_default_path)
+		lo.Must0(err)
+	}
 }
 
 // executable returns lo.Must1(os.Executable())
--- util/util.go
+++ util/util.go
@@ -1,1 +1,57 @@
 package util
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/samber/lo"
+)
+
+// WrapErrors 把多个错误合并为一个错误.
+func WrapErrors(allErrors ...error) (wrapped error) {
+	for _, err := range allErrors {
+		if err != nil {
+			if wrapped == nil {
+				wrapped = err
+			} else {
+				wrapped = fmt.Errorf("%w | %w", wrapped, err)
+			}
+		}
+	}
+	return
+}
+
+// TODO: 改名 PathNotExists
+func PathNotExists(name string) (ok bool) {
+	_, err := os.Lstat(name)
+	if os.IsNotExist(err) {
+		ok = true
+		err = nil
+	}
+	lo.Must0(err)
+	return
+}
+
+func PathExists(name string) bool {
+	return !PathNotExists(name)
+}
+
+// https://stackoverflow.com/questions/30376921/how-do-you-copy-a-file-in-go
+func CopyFile(dstPath, srcPath string) error {
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err1 := io.Copy(dst, src)
+	err2 := dst.Sync()
+	return WrapErrors(err1, err2)
+}
